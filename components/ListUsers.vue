9a14d8a43220e6f6d98abe4bcd342da559a7187f
Jose eneas <ninho.serafim95@gmail.com>
2020-08-24 19:03:49 -0300

    projeto criado

--- components/ListUsers.vue
+++ components/ListUsers.vue
@@ -1,18 +1,17 @@
 <template>
   
-<div class="container">
-
+  <div class="container">
     <v-list >
       <v-list-title>
         <h2>Users</h2>
       </v-list-title> 
       <v-list-item v-for ="user in pessoas">
         <v-list-item-content>
-            <v-row justify="space-around">
-              <v-avatar size="62" class="bottom">
-               <img src="https://library.kissclipart.com/20180916/zvq/kissclipart-icon-clipart-computer-icons-kerona-scientific-limi-a1bc81c60735cb88.jpg">
-              </v-avatar>
-            </v-row>  
+          <v-row justify="space-around">
+            <v-avatar size="62" class="bottom">
+              <img src="https://library.kissclipart.com/20180916/zvq/kissclipart-icon-clipart-computer-icons-kerona-scientific-limi-a1bc81c60735cb88.jpg">
+            </v-avatar>
+          </v-row>  
           <v-list-item-title>{{user.id }} - {{ user.name }}</v-list-item-title>
           <v-list-item-subtitle>{{user.email}}</v-list-item-subtitle>
           <v-list-item-subtitle>{{user.address.street}},{{user.address.city}}</v-list-item-subtitle>
@@ -31,13 +30,12 @@
         pessoas: []
         }
       },
-   async mounted() {
+    async mounted() {
       this.pessoas = await this.getUsers()
     },
     methods: {
       async getUsers() {
         const { data }  = await users.listar()
-        
          return data
       }
     }
@@ -50,11 +48,11 @@
     margin-top: 20px;
     font-family: sans-serif;  
   }
-.line{
- margin-top: 20px;
+  .line{
+    margin-top: 20px;
 
-}
-.bottom{
-  margin-bottom:20px;
-}
+  }
+  .bottom{
+   margin-bottom:20px;
+  }
 </style>
